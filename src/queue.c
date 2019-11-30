b6643b5d05959aa9b69a331a014e4085531634ac
Anas Rchid <archid-@student.1337.ma>
2019-11-30 08:58:54 +0100

    add a basic queue data structure impl

---
+++ src/queue.c
@@ -0,0 +1,167 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   queue.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: archid- <archid-@student.1337.ma>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/29 17:12:11 by archid-           #+#    #+#             */
+/*   Updated: 2019/11/30 08:57:26 by archid-          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "queue.h"
+
+t_qnode		*queue_node(void *blob, size_t size)
+{
+	t_qnode *node;
+
+	node = (t_qnode *)malloc(sizeof(t_qnode));
+	node->size = blob ? size : 0;
+	node->blob = blob ? ft_memcpy(malloc(size), blob, size) : NULL;
+	node->next = NULL;
+	node->prev = NULL;
+	return (node);
+}
+
+void		queue_node_del(t_qnode **a_node, void (*del)(void *, size_t))
+{
+	if (!a_node || !*a_node)
+		return ;
+	(*del)((*a_node)->blob, (*a_node)->size);
+	free(*a_node);
+	*a_node = NULL;
+}
+
+t_queue		*queue_init(void)
+{
+	t_queue *queue;
+
+	queue = (t_queue *)malloc(sizeof(t_queue));
+	queue->head = queue_node(NULL, 0);
+	queue->tail = queue_node(NULL, 0);
+	queue->head->next = queue->tail;
+	queue->tail->prev = queue->head;
+	return (queue);
+}
+
+size_t		queue_size(t_queue *q)
+{
+	size_t size;
+	t_qnode *walk;
+
+	if (!q || q->head->next == q->tail)
+		return (0);
+	size = 0;
+	walk = q->head->next;
+	while (walk)
+	{
+		size++;
+		walk = walk->next;
+	}
+	return (size);
+}
+
+void		queue_del(t_queue **q, void (*del)(void *, size_t))
+{
+	t_qnode *walk;
+	t_qnode *tmp;
+
+	if (!q)
+		return ;
+
+	walk = (*q)->head->next;
+	while (walk != (*q)->tail)
+	{
+		tmp = walk;
+		walk = walk->next;
+		queue_node_del(&tmp, del);
+	}
+	queue_node_del(&(*q)->head, del);
+	queue_node_del(&(*q)->tail, del);
+	free(*q);
+	*q = NULL;
+
+}
+
+void		queue_enq(t_queue *queue, t_qnode *node)
+{
+	if (!queue || !node)
+		return ;
+	queue->tail->prev->next = node;
+	node->prev = queue->tail->prev;
+	node->next = queue->tail;
+	queue->tail->prev = node;
+}
+
+t_qnode 	*queue_deq(t_queue *queue)
+{
+	t_qnode *node;
+
+	if (!queue || queue->head->next == queue->tail)
+		return (NULL);
+	node = queue->head->next;
+	queue->head->next->prev = queue->head;
+	queue->head->next = node->next;
+	return (node);
+}
+
+void		queue_iter(t_queue *q, void (*f)(t_qnode *))
+{
+	t_qnode *walk;
+
+	if (!q || q->head->next == q->tail)
+		return ;
+	walk = q->head->next;
+	while (walk != q->tail)
+	{
+		(*f)(walk);
+		walk = walk->next;
+	}
+}
+
+/*
+void		print_int_node(t_qnode *node)
+{
+	if (!node)
+		return ;
+	ft_printf("%d ", *(int *)node->blob);
+
+}
+
+void		queue_del_helper(void *blob, size_t size)
+{
+	if (size)
+		free(blob);
+}
+
+int			main(int ac, char **av)
+{
+	int		i;
+	int		val;
+	t_queue *q;
+	t_qnode *node;
+
+	if (ac == 1)
+		return -1;
+	i = 1;
+	q = queue_init();
+	while (i < ac)
+	{
+		val = ft_atoi(av[i++]);
+		queue_enq(q, queue_node(&val, sizeof(int)));
+		queue_iter(q, print_int_node);
+		ft_printf("\nsize >> %zu\n", queue_size(q));
+	}
+	ft_putchar('\n');
+	while(queue_size(q))
+	{
+		queue_iter(q, print_int_node);
+		node = queue_deq(q);
+		queue_node_del(&node, queue_del_helper);
+		ft_putchar('\n');
+	}
+	queue_del(&q, queue_del_helper);
+	return 0;
+}
+*/
---
+++ src/queue.h
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   queue.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: archid- <archid-@student.1337.ma>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/29 16:34:27 by archid-           #+#    #+#             */
+/*   Updated: 2019/11/30 08:29:44 by archid-          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef QUEUE_H
+# define QUEUE_H
+
+# include "libft.h"
+
+typedef struct s_queue_node	t_qnode;
+struct							s_queue_node
+{
+	void				*blob;
+	size_t				size;
+	struct s_queue_node	*next;
+	struct s_queue_node	*prev;
+};
+
+/*
+** always head -> tail
+** inserting nodes between head and tail
+*/
+
+typedef struct s_queue			t_queue;
+struct							s_queue
+{
+	struct s_queue_node *head;
+	struct s_queue_node *tail;
+	void				(*del)(void *, size_t);
+};
+
+t_qnode							*queue_node(void *blob, size_t size);
+void							queue_node_del(t_qnode **a_node,
+												void (*del)(void *, size_t));
+
+t_queue							*queue_init(void);
+void							queue_del(t_queue **a_queue,
+											void (*del)(void *, size_t));
+
+void							queue_enq(t_queue *queue, t_qnode *node);
+t_qnode 						*queue_deq(t_queue *queue);
+
+#endif
