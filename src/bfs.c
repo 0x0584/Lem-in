b6467260356a3a1549f01a1df396ce8cd172cf1e
Anas Rchid <archid-@student.1337.ma>
2020-11-14 17:16:31 +0100

    REFACTOR: spliting rewire into smaller functions

--- src/bfs.c
+++ src/bfs.c
@@ -6,7 +6,7 @@
 /*   By: melalj <melalj@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/30 09:00:42 by archid-           #+#    #+#             */
-/*   Updated: 2020/11/14 16:46:45 by archid-          ###   ########.fr       */
+/*   Updated: 2020/11/14 17:15:51 by archid-          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,17 @@
 */
 
 static int g_turn = 2;
+
+struct s_rewire_handy {
+    t_queue **apath;     /* keeping track of each path */
+    t_qnode **walk_edge; /* moving through the paths one edge at a time */
+    size_t n_paths;      /* number of paths */
+    size_t curr;         /* walk index */
+
+    /* edge pair used while walking */
+    t_qnode *e1;
+    t_qnode *e2;
+};
 
 static t_queue *bfs_helper(t_graph *g, t_edge **parent) {
     t_queue *path;
@@ -90,27 +101,28 @@ t_queue *bfs_find(t_graph *g) {
     return path;
 }
 
-void sort_by_node_src_name(t_queue **base, t_qnode **node, size_t size) {
+void sort_by_node_src_name(struct s_rewire_handy *info) {
 
     size_t i;
     size_t j;
     t_queue *q;
     t_qnode *e;
 
-    if (!base || !node || !size)
+    if (!info->apath || !info->walk_edge || !info->n_paths)
         return;
     i = 0;
-    while (i < size - 1) {
+    while (i < info->n_paths - 1) {
         j = 0;
-        while (j < size - 1) {
-            if (ft_strcmp(AS_EDGE(node[j])->node_src->name,
-                          AS_EDGE(node[j + 1])->node_src->name) > 0) {
-                q = base[j];
-                base[j] = base[j + 1];
-                base[j + 1] = q;
-                e = node[j];
-                node[j] = node[j + 1];
-                node[j + 1] = e;
+        while (j < info->n_paths - 1) {
+            if (ft_strcmp(AS_EDGE(info->walk_edge[j])->node_src->name,
+                          AS_EDGE(info->walk_edge[j + 1])->node_src->name) >
+                0) {
+                q = info->apath[j];
+                info->apath[j] = info->apath[j + 1];
+                info->apath[j + 1] = q;
+                e = info->walk_edge[j];
+                info->walk_edge[j] = info->walk_edge[j + 1];
+                info->walk_edge[j + 1] = e;
             }
             j++;
         }
@@ -124,46 +136,45 @@ static bool has_arrived(t_graph *g, t_edge *e) {
     return (g->start == e->node_src);
 }
 
-static t_qnode *next_edge(t_graph *g, t_queue *q, t_qnode *edge) {
+static t_qnode *next_edge(t_graph *g, t_qnode *edge) {
     if (!g || !edge)
         return NULL;
     return has_arrived(g, QNODE_AS(struct s_edge, edge)) ? edge : edge->next;
 }
 
-static void set_walk_edges(t_queue *paths, t_queue **apath,
-                           t_qnode **walk_edge) {
+static void set_walk_edges(t_queue *paths, struct s_rewire_handy *info) {
     t_qnode *walk;
     size_t curr;
 
     walk = paths->head->next;
     curr = 0;
     while (walk != paths->tail) {
-        apath[curr] = walk->blob;
-        walk_edge[curr] = apath[curr]->head->next;
+        info->apath[curr] = walk->blob;
+        info->walk_edge[curr] = info->apath[curr]->head->next;
         curr++;
         walk = walk->next;
     }
 }
 
-bool detect_collition(t_graph *g, t_qnode *e1, t_qnode *e2, size_t curr,
-                      t_qnode **walk_edge) {
+bool detect_collition(t_graph *g, struct s_rewire_handy *info) {
     /* looking for edges that cae from the same node */
     return /* #1 check current edges first, curr and curr + 1 */
-        (!has_arrived(g, AS_EDGE(walk_edge[curr])) &&
-         !ft_strcmp(AS_EDGE(walk_edge[curr])->node_src->name,
-                    AS_EDGE(walk_edge[curr + 1])->node_src->name))
+        (!has_arrived(g, AS_EDGE(info->walk_edge[info->curr])) &&
+         !ft_strcmp(AS_EDGE(info->walk_edge[info->curr])->node_src->name,
+                    AS_EDGE(info->walk_edge[info->curr + 1])->node_src->name))
         /* 2# or either path 2 has an edge that does */
-        || (e2 && !has_arrived(g, AS_EDGE(e2)) &&
-            !ft_strcmp(AS_EDGE(walk_edge[curr])->node_src->name,
-                       AS_EDGE(e2)->node_src->name))
+        || (info->e2 && !has_arrived(g, AS_EDGE(info->e2)) &&
+            !ft_strcmp(AS_EDGE(info->walk_edge[info->curr])->node_src->name,
+                       AS_EDGE(info->e2)->node_src->name))
         /* 3# path 1 is the one who does  */
-        || (e1 && !has_arrived(g, AS_EDGE(e2)) &&
-            !ft_strcmp(AS_EDGE(e1)->node_src->name,
-                       AS_EDGE(walk_edge[curr + 1])->node_src->name));
+        ||
+        (info->e1 && !has_arrived(g, AS_EDGE(info->e2)) &&
+         !ft_strcmp(AS_EDGE(info->e1)->node_src->name,
+                    AS_EDGE(info->walk_edge[info->curr + 1])->node_src->name));
 }
 
-bool discover_collition(t_graph *g, t_qnode *e1, t_qnode *e2, size_t curr,
-                        t_qnode **walk_edge, t_queue **apath, int *residual) {
+bool discover_collition(t_graph *g, struct s_rewire_handy *info,
+                        int *residual) {
     t_qnode *after1;
     t_qnode *after2;
 
@@ -172,90 +183,115 @@ bool discover_collition(t_graph *g, t_qnode *e1, t_qnode *e2, size_t curr,
       it to NULL and test afterwards!
     */
 
-    if (e1 == (after1 = next_edge(g, apath[curr], e1)))
+    if (info->e1 == (after1 = next_edge(g, info->e1)))
         after1 = NULL;
-    if (e2 == (after2 = next_edge(g, apath[curr + 1], e2)))
+    if (info->e2 == (after2 = next_edge(g, info->e2)))
         after2 = NULL;
 
     /* preparing target edges */
     bool move_edge;
 
     move_edge = false;
-    if (e2 && AS_EDGE(walk_edge[curr])->residual == AS_EDGE(e2))
+    if (info->e2 &&
+        AS_EDGE(info->walk_edge[info->curr])->residual == AS_EDGE(info->e2))
         /* residual by default in this case would be path2
          * have the residual right after one */
         *residual = 1;
-    else if (after2 && AS_EDGE(after2)->residual == AS_EDGE(walk_edge[curr])) {
+    else if (after2 && AS_EDGE(after2)->residual ==
+                           AS_EDGE(info->walk_edge[info->curr])) {
         /* however, in this case, the residual in the path1 itself, since after
          * two nodes, we have found the residual */
         *residual = 0;
-        walk_edge[curr + 1] = after2;
+        info->walk_edge[info->curr + 1] = after2;
         move_edge = true;
-    } else if (after1 &&
-               AS_EDGE(after1)->residual == AS_EDGE(walk_edge[curr + 1])) {
+    } else if (after1 && AS_EDGE(after1)->residual ==
+                             AS_EDGE(info->walk_edge[info->curr + 1])) {
         *residual = 1;
-        walk_edge[curr] = after1;
+        info->walk_edge[info->curr] = after1;
         move_edge = true;
     } else
         *residual = 0;
     return move_edge;
 }
 
-void fix_collition(t_graph *g, t_qnode *e1, t_qnode *e2, size_t curr,
-                   t_qnode **walk_edge, t_queue **apath) {
+void fix_collition(t_graph *g, struct s_rewire_handy *info) {
     int residual;
-    bool move_edge =
-        discover_collition(g, e1, e2, curr, walk_edge, apath, &residual);
+    bool move_edge = discover_collition(g, info, &residual);
 
     if (move_edge)
-        walk_edge[curr + residual] = walk_edge[curr + residual]->prev;
+        info->walk_edge[info->curr + residual] =
+            info->walk_edge[info->curr + residual]->prev;
 
-    walk_edge[curr + !residual] = walk_edge[curr + !residual]->prev;
+    info->walk_edge[info->curr + !residual] =
+        info->walk_edge[info->curr + !residual]->prev;
 
-    t_qnode *old_prev = walk_edge[curr + !residual]->prev;
+    t_qnode *old_prev = info->walk_edge[info->curr + !residual]->prev;
 
     /* remove residual edge from its path */
-    queue_node_del_next(apath[curr + residual], walk_edge[curr + residual],
+    queue_node_del_next(info->apath[info->curr + residual],
+                        info->walk_edge[info->curr + residual],
                         queue_node_del_dry);
-    walk_edge[curr + residual] = walk_edge[curr + residual]->next;
+    info->walk_edge[info->curr + residual] =
+        info->walk_edge[info->curr + residual]->next;
 
     /* removing the edge itself from its none residual path */
-    queue_node_del_next(apath[curr + !residual], walk_edge[curr + !residual],
+    queue_node_del_next(info->apath[info->curr + !residual],
+                        info->walk_edge[info->curr + !residual],
                         queue_node_del_dry);
 
-    t_qnode *old_next = walk_edge[curr + !residual]->next;
+    t_qnode *old_next = info->walk_edge[info->curr + !residual]->next;
 
     /* merge the other half of each path a-b 0-9 => a-9 0-b */
-    queue_swap_halfs(apath[curr + !residual], apath[curr + residual],
-                     walk_edge[curr + !residual], walk_edge[curr + residual]);
-    walk_edge[curr + !residual] = old_next;
+    queue_swap_halfs(info->apath[info->curr + !residual],
+                     info->apath[info->curr + residual],
+                     info->walk_edge[info->curr + !residual],
+                     info->walk_edge[info->curr + residual]);
+    info->walk_edge[info->curr + !residual] = old_next;
 
     if (move_edge)
-        walk_edge[curr + residual] = old_prev;
+        info->walk_edge[info->curr + residual] = old_prev;
+}
+
+bool prepare_info(struct s_rewire_handy *info, t_queue *paths) {
+    if (!paths || !(info->n_paths = queue_size(paths)))
+        return NULL;
+
+    info->apath = malloc(info->n_paths * sizeof(t_queue *));
+    info->walk_edge = malloc(info->n_paths * sizeof(t_qnode *));
+    /* setup all found paths */
+    set_walk_edges(paths, info);
+    return true;
+}
+
+void pick_pair(t_graph *g, struct s_rewire_handy *info) {
+    info->e1 = next_edge(g, info->walk_edge[info->curr]);
+    info->e2 = next_edge(g, info->walk_edge[info->curr + 1]);
+}
+
+void walk_edges(t_graph *g, struct s_rewire_handy *info) {
+    info->curr = 0;
+    while (info->curr < info->n_paths) {
+        /* ignoring paths that have reached the source */
+        if (AS_EDGE(info->walk_edge[info->curr])->node_src != g->start)
+            info->walk_edge[info->curr] = info->walk_edge[info->curr]->next;
+        info->curr++;
+    }
 }
 
 t_queue *re_wire_paths(t_graph *g, t_queue *paths) {
-    t_queue **apath;     /* keeping track of each path */
-    t_qnode **walk_edge; /* moving through the paths one edge at a time */
-    size_t n_paths;
-    size_t curr;
+    struct s_rewire_handy info;
     bool collision;
-
-    if (!paths || !(n_paths = queue_size(paths)))
-        return NULL;
-    curr = 0;
-    apath = malloc(n_paths * sizeof(t_queue *));
-    walk_edge = malloc(n_paths * sizeof(t_qnode *));
-    /* setup all found paths */
-    set_walk_edges(paths, apath, walk_edge);
-
     bool done;
+
+    if (!prepare_info(&info, paths))
+        return NULL;
+
     while (true) {
         /* if all paths have arrived, that means edge is all NULLs */
-        curr = 0;
+        info.curr = 0;
         done = true;
-        while (curr < n_paths && done)
-            if (AS_EDGE(walk_edge[curr++])->node_src != g->start)
+        while (info.curr < info.n_paths && done)
+            if (AS_EDGE(info.walk_edge[info.curr++])->node_src != g->start)
                 done = false;
         if (done)
             break;
@@ -265,39 +301,27 @@ t_queue *re_wire_paths(t_graph *g, t_queue *paths) {
 
         /* moving one edge on each path */
 
-        sort_by_node_src_name(apath, walk_edge, n_paths);
-        curr = 0;
+        sort_by_node_src_name(&info);
+        info.curr = 0;
         collision = false;
-        while (curr < n_paths - 1) {
-            t_qnode *e1;
-            t_qnode *e2;
-
-            e1 = next_edge(g, apath[curr], walk_edge[curr]);
-            e2 = next_edge(g, apath[curr + 1], walk_edge[curr + 1]);
-            if (detect_collition(g, e1, e2, curr, walk_edge)) {
-                fix_collition(g, e1, e2, curr, walk_edge, apath);
+        while (info.curr < info.n_paths - 1) {
+            pick_pair(g, &info);
+            if (detect_collition(g, &info)) {
+                fix_collition(g, &info);
                 collision = true;
                 break;
             }
-            curr++;
+            info.curr++;
         }
 
         if (collision) {
-            set_walk_edges(paths, apath,
-                           walk_edge); /* start from the sink again */
+            set_walk_edges(paths, &info); /* start from the sink again */
             continue;
         }
-
-        curr = 0;
-        while (curr < n_paths) {
-            /* ignoring paths that have reached the source */
-            if (AS_EDGE(walk_edge[curr])->node_src != g->start)
-                walk_edge[curr] = walk_edge[curr]->next;
-            curr++;
-        }
+        walk_edges(g, &info);
     }
 
-    free(apath);
-    free(walk_edge);
+    free(info.apath);
+    free(info.walk_edge);
     return (paths);
 }
