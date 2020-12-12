31df0eb53c89c47a083cc6682c8c40ca8399465c
Anas Rchid <archid-@student.1337.ma>
2020-12-12 13:41:57 +0100

    fix double free in correct_path

--- src/correction.c
+++ src/correction.c
@@ -6,7 +6,7 @@
 /*   By: archid- <archid-@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/26 23:46:38 by archid-           #+#    #+#             */
-/*   Updated: 2020/12/12 00:55:41 by archid-          ###   ########.fr       */
+/*   Updated: 2020/12/12 13:41:24 by archid-          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,13 @@ int shortest_path(void *path_a, void *path_b) {
         return -1;
     else
         return 1;
+}
+
+void print_edge_show_residual(void *blob) {
+    t_edge e = blob;
+
+    ft_printf("%s-%s%{red_fg}%s%{reset} ", e->src->name, e->dst->name,
+              e->residual->seen == M_BELONG_TO_PATH ? "*" : "");
 }
 
 void print_path(void *path) {
@@ -48,11 +55,64 @@ static bool residual_of(t_edge e, t_edge re) { return re == e->residual; }
 
 static void set_edge_fresh(t_edge e) { e->seen = M_FRESH; }
 
-static void swap_paths(t_lstnode path_a, t_lstnode path_b) {
-    path_a->next->prev = path_b->prev;
-    path_b->prev->next = path_a->next;
-    path_a->next = path_b;
-    path_b->prev = path_a;
+static void swap_paths(t_lst path_a, t_lstnode walk_path_a, t_lst path_b,
+                       t_lstnode walk_path_b) {
+    t_lstnode tmp;
+    t_lst front;
+
+    ft_putendl("/////////////////////////////////////////");
+    front = lst_alloc(blob_keep);
+
+    while (lst_front(path_b) != walk_path_b)
+        lst_push_back(front, lst_pop_front(path_b));
+
+    {
+        ft_printf(" %{red_fg}>>> ----- front path --------%{reset}\n");
+        print_path(front);
+    }
+
+    tmp = lst_rear(front);
+    while (lst_rear(path_a) != walk_path_a)
+        lst_insert_after(front, tmp, lst_pop_back(path_a));
+
+    {
+        ft_printf(" %{red_fg}>>> ----- front path 2 --------%{reset}\n");
+        print_path(front);
+    }
+
+    {
+        ft_printf(" %{red_fg}>>> -----  path a --------%{reset}\n");
+        print_path(path_a);
+    }
+
+    {
+        ft_printf(" %{red_fg}>>> -----  path b --------%{reset}\n");
+        print_path(path_b);
+    }
+    ft_putendl("/////////////////////////////////////////");
+
+    while (!lst_empty(path_b))
+        lst_push_back(path_a, lst_pop_front(path_b));
+
+    while (!lst_empty(front))
+        lst_push_back(path_b, lst_pop_front(front));
+
+    lst_del(&front);
+
+    {
+        ft_printf(" %{red_fg}>>> -----  PATH A --------%{reset}\n");
+        print_path(path_a);
+    }
+
+    {
+        ft_printf(" %{red_fg}>>> -----  PATH B --------%{reset}\n");
+        print_path(path_b);
+    }
+
+    /* path_a->next->prev = path_b->prev; */
+    /* path_b->prev->next = path_a->next; */
+    /* path_a->next = path_b; */
+    /* path_b->prev = path_a; */
 }
 
 static void correct_path(t_lst path_a, t_lst paths) {
@@ -62,6 +122,10 @@ static void correct_path(t_lst path_a, t_lst paths) {
     t_lstnode tmp;
     bool has_residual;
 
+    {
+        ft_printf(" %{red_fg}>>> ----- in path --------%{reset}\n");
+        print_path(path_a);
+    }
     walk_a = lst_front(path_a);
     while (walk_a) {
         while (walk_a && !residual(walk_a->blob))
@@ -73,6 +137,11 @@ static void correct_path(t_lst path_a, t_lst paths) {
                 lst_node_forward(&path_b);
                 continue;
             }
+            {
+                ft_printf(
+                    " %{yellow_fg}>>> ----- current path --------%{reset}\n");
+                print_path(path_b->blob);
+            }
             has_residual = false;
             walk_b = lst_front(path_b->blob);
             while (walk_b && !residual_of(walk_a->blob, walk_b->blob)) {
@@ -82,12 +151,21 @@ static void correct_path(t_lst path_a, t_lst paths) {
             if (walk_b) {
                 lst_node_backward(&walk_a), lst_node_forward(&walk_b);
                 while (residual_of(walk_a->next->blob, walk_b->prev->blob)) {
+                    {
+                        ft_printf(" %{red_fg}>>> ----- path_a "
+                                  "--------%{reset}\n");
+                        print_path(path_a);
+                        ft_printf(" %{yellow_fg}>>> ----- path_b "
+                                  "--------%{reset}\n");
+                        print_path(path_b->blob);
+                        ft_putendl(" ######### ");
+                    }
                     set_edge_fresh(walk_a->next->blob);
                     set_edge_fresh(walk_b->prev->blob);
                     lst_remove_next(path_a, walk_a);
                     lst_remove_previous(path_b->blob, walk_b);
                 }
-                swap_paths(walk_a, walk_b);
+                swap_paths(path_a, walk_a, path_b->blob, walk_b);
                 break;
             }
             tmp = has_residual ? NULL : path_b;
@@ -118,8 +196,11 @@ void correct_paths(t_lst paths) {
 
     lst_del(&paths2);
     {
-        ft_printf("\n%{green_fg}paths after%{reset}\n");
+        ft_printf(" %{cyan_fg}>>> ----- original paths --------%{reset}\n");
         lst_iter(paths, true, print_path);
+        ft_printf(" %{magenta_fg}>>> ----- paths copy --------%{reset}\n");
+        lst_iter(paths2, true, print_path);
+        ft_putendl(" >>> -------- -----");
         assert_paths_correct(paths);
     }
 }
